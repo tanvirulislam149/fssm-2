e17434f69ee532caa6af6b3ec9c4913c0f1e40ef
joshua <joshuadavidoni@yahoo.com>
2022-07-22 20:55:18 +0100

    new commit

--- components/TextArrays.js
+++ components/TextArrays.js
@@ -1,5 +1,6 @@
 export const loginFormText = {
   header_text_1: 'SWACHH',
   header_text_2: 'FSSM',
-  remeber_me: 'Remember me'
+  remeber_me: 'Remember me',
+  forgot_passwword: 'Forgot Password?'
 }
