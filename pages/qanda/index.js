b44939975a6a0610842cee005657630509b10e40
joshua <joshuadavidoni@yahoo.com>
2022-07-25 13:09:37 +0100

    added help desk route

---
+++ pages/qanda/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../../components/Headers/Header';
+import HeaderComponent from '../../components/Headers/HeaderComponent';
+import Footer from '../../components/Footers/Footer';
+import styles from './HelpDesk.module.css';
+
+const HelpDesk = () => {
+  return (
+    <>
+      <Header />
+      <HeaderComponent />
+
+      <Footer />
+    </>
+  )
+}
+
+export default HelpDesk
