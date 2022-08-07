e3485148090587e5f6cd03b7eb6439c504f109b7
joshua <joshuadavidoni@yahoo.com>
2022-08-07 17:13:22 +0100

    update

--- components/Sections/Carousel/Carousel.js
+++ components/Sections/Carousel/Carousel.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './Carousel.module.css';
 import Button from '../../Buttons/Submit/SubmitButton';
 import { homePageMidSectionText } from '../../TextArrays';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Carousel = () => {
+  const router = useRouter();
+
+  const id = router.asPath.slice(router.asPath.length - 1);
+
+  useEffect(() => {
+    document.getElementById(`carousel_${id}`).classList.add('carousel_select');
+  }, [])
+
+  const handleChange = (el) => {
+    document.getElementById('carousel_1').classList.remove('carousel_select');
+    document.getElementById('carousel_2').classList.remove('carousel_select');
+    document.getElementById('carousel_3').classList.remove('carousel_select');
+    document.getElementById('carousel_4').classList.remove('carousel_select');
+    el.classList.add('carousel_select');
+  }
+
+
   return (
     <>
       <div className={styles.container}>
@@ -26,8 +44,14 @@ const Carousel = () => {
                   <Button title='Show more' style={styles.btn} />
                 </div>
                 <a href="#carousel__slide4"
+                  onClick={() => {
+                    handleChange(document.getElementById('carousel_4'));
+                  }}
                   className={styles.carousel__prev}>&lt;</a>
                 <a href="#carousel__slide2"
+                  onClick={() => {
+                    handleChange(document.getElementById('carousel_2'));
+                  }}
                   className={styles.carousel__next}>&gt;</a>
               </div>
             </li>
@@ -47,8 +71,14 @@ const Carousel = () => {
                 <Button title='Show more' style={styles.btn} />
               </div>
               <a href="#carousel__slide1"
+                onClick={() => {
+                  handleChange(document.getElementById('carousel_1'));
+                }}
                 className={styles.carousel__prev}>&lt;</a>
               <a href="#carousel__slide3"
+                onClick={() => {
+                  handleChange(document.getElementById('carousel_3'));
+                }}
                 className={styles.carousel__next}>&gt;</a>
             </li>
             <li id="carousel__slide3"
@@ -67,8 +97,14 @@ const Carousel = () => {
                 <Button title='Show more' style={styles.btn} />
               </div>
               <a href="#carousel__slide2"
+                onClick={() => {
+                  handleChange(document.getElementById('carousel_2'));
+                }}
                 className={styles.carousel__prev}>&lt;</a>
               <a href="#carousel__slide4"
+                onClick={() => {
+                  handleChange(document.getElementById('carousel_4'));
+                }}
                 className={styles.carousel__next}>&gt;</a>
             </li>
             <li id="carousel__slide4"
@@ -87,27 +123,45 @@ const Carousel = () => {
                 <Button title='Show more' style={styles.btn} />
               </div>
               <a href="#carousel__slide3"
+                onClick={() => {
+                  handleChange(document.getElementById('carousel_3'));
+                }}
                 className={styles.carousel__prev}>&lt;</a>
               <a href="#carousel__slide1"
+                onClick={() => {
+                  handleChange(document.getElementById('carousel_1'));
+                }}
                 className={styles.carousel__next}>&gt;</a>
             </li>
           </ol>
           <aside className={styles.carousel__navigation}>
             <ol className={styles.carousel__navigation_list}>
               <li className={styles.carousel__navigation_item}>
-                <a href="#carousel__slide1"
+                <a href="#carousel__slide1" id='carousel_1'
+                  onClick={() => {
+                    handleChange(document.getElementById('carousel_1'))
+                  }}
                   className={styles.carousel__navigation_button}></a>
               </li>
               <li className={styles.carousel__navigation_item}>
-                <a href="#carousel__slide2"
+                <a href="#carousel__slide2" id='carousel_2'
+                  onClick={() => {
+                    handleChange(document.getElementById('carousel_2'))
+                  }}
                   className={styles.carousel__navigation_button}></a>
               </li>
               <li className={styles.carousel__navigation_item}>
-                <a href="#carousel__slide3"
+                <a href="#carousel__slide3" id='carousel_3'
+                  onClick={() => {
+                    handleChange(document.getElementById('carousel_3'))
+                  }}
                   className={styles.carousel__navigation_button}></a>
               </li>
               <li className={styles.carousel__navigation_item}>
-                <a href="#carousel__slide4"
+                <a href="#carousel__slide4" id='carousel_4'
+                  onClick={() => {
+                    handleChange(document.getElementById('carousel_4'))
+                  }}
                   className={styles.carousel__navigation_button}></a>
               </li>
             </ol>
