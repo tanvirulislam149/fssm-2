00ffd62c0491c6856a45c94deba628cb7283e031
joshua <joshuadavidoni@yahoo.com>
2022-08-19 19:21:19 +0100

    client fixes

--- components/Sections/AboutDescription/AboutDescription.js
+++ components/Sections/AboutDescription/AboutDescription.js
@@ -22,15 +22,15 @@ const AboutDescription = () => {
 
           In a first of its kind, this one-stop knowledge platform is intended to support practitioners to advance FSSM across India at all levels including national, state and city. This platform has many uniquely curated features and knowledge management options including: <br /><br />
 
-          • <span className={styles.links}><Link href='/'>Quick and Advanced Search;</Link></span><br />
+          • <span className={styles.links}><Link href='/'>Quick and Advanced Search;</Link></span><br /><br />
 
-          • <span className={styles.links}><Link href='/knowledgecontent'>Curated knowledge guides for different user categories;</Link></span><br />
+          • <span className={styles.links}><Link href='/knowledgecontent'>Curated knowledge guides for different user categories;</Link></span><br /><br />
 
-          • <span className={styles.links}><Link href='/qanda'>A Help Desk to have all your queries answered directly by Subject Matter Experts within 5 days;</Link></span><br />
+          • <span className={styles.links}><Link href='/qanda'>A Help Desk to have all your queries answered directly by Subject Matter Experts within 5 days;</Link></span><br /><br />
 
-          • <span className={styles.links}><Link href='/faq'>FAQs and Glossary for quick referencing;</Link></span><br />
+          • <span className={styles.links}><Link href='/faq'>FAQs and Glossary for quick referencing;</Link></span><br /><br />
 
-          • <span className={styles.links}><Link href='/'>Tenders to keep informed and updated;</Link></span><br />
+          • <span className={styles.links}><Link href='/'>Tenders to keep informed and updated;</Link></span><br /><br />
 
           For more information, please contact <a href='mailto:info@swachhfssm.in'>info@swachhfssm.in.</a></p>
       </div>
