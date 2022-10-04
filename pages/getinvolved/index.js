e2379582a59822bbaa5135dfffa043ac389d0a5f
tanvir <tanvirulislam149@gmail.com>
2022-10-04 23:15:46 +0600

    route setup done

---
+++ pages/getinvolved/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import DashboardHeader from "../../components/Dashboard/Header/DashboardHeader";
+import SideBar from "../../components/Dashboard/SideBar/SideBar";
+import styles from "../../styles/Home.module.css";
+import Layout from "../../components/Sections/Layout/Layout"
+import HelpDesk from "../../components/Dashboard/HelpDesk/HelpDesk";
+import GetInvolved from "../../components/Dashboard/GetInvolved/GetInvolved";
+
+const GetInvolvedPage = () => {
+  return (
+    <>
+      <Layout>
+        <DashboardHeader />
+        <div className={styles.dash_cont}>
+          <div>
+            <SideBar />
+          </div>
+          <div>
+            <GetInvolved />
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default GetInvolvedPage;
