640d645317665f1944c243d52f0a7b087270cf48
joshua <joshuadavidoni@yahoo.com>
2022-10-07 16:45:00 +0100

    fixes

--- middleware.js
+++ middleware.js
@@ -4,7 +4,13 @@ export default function middleware(req) {
   const { url } = req;
   const isAdmin = req.cookies.get('isAdmin');
   const { origin } = req.nextUrl;
-  if (url.includes('/dashboard')) {
+  if (url.includes('/dashboard') || url.includes('/bulkupload') ||
+    url.includes('/documentsmapping') || url.includes('/mydocuments') ||
+    url.includes('/documents') || url.includes('/documentsdump') ||
+    url.includes('/entity') || url.includes('/org') ||
+    url.includes('/docu') || url.includes('/qanda-admin') ||
+    url.includes('/forum') || url.includes('/faq-admin') ||
+    url.includes('/getinvolved') || url.includes('/analytics')) {
     if (!isAdmin) {
       return NextResponse.rewrite(`${origin}/signin`)
     }
