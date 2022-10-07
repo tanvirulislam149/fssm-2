4c57744f4dad9aa77676aced4ef9395be928af96
joshua <joshuadavidoni@yahoo.com>
2022-10-07 20:19:15 +0100

    fixes

--- middleware.js
+++ middleware.js
@@ -4,17 +4,17 @@ export default function middleware(req) {
   const { url } = req;
   const isAdmin = req.cookies.get('isAdmin');
   const { origin } = req.nextUrl;
-  if (url.includes('/dashboard') || url.includes('/bulkupload') ||
-    url.includes('/documentsmapping') || url.includes('/mydocuments') ||
-    url.includes('/documents') || url.includes('/documentsdump') ||
-    url.includes('/entity') || url.includes('/org') ||
-    url.includes('/docu') || url.includes('/qanda-admin') ||
-    url.includes('/forum') || url.includes('/faq-admin') ||
-    url.includes('/getinvolved') || url.includes('/analytics')) {
-    if (!isAdmin) {
-      return NextResponse.rewrite(`${origin}/signin`)
-    }
-  }
+  // if (url.includes('/dashboard') || url.includes('/bulkupload') ||
+  //   url.includes('/documentsmapping') || url.includes('/mydocuments') ||
+  //   url.includes('/documents') || url.includes('/documentsdump') ||
+  //   url.includes('/entity') || url.includes('/org') ||
+  //   url.includes('/docu') || url.includes('/qanda-admin') ||
+  //   url.includes('/forum') || url.includes('/faq-admin') ||
+  //   url.includes('/getinvolved') || url.includes('/analytics')) {
+  //   if (!isAdmin) {
+  //     return NextResponse.rewrite(`${origin}/signin`)
+  //   }
+  // }
 
   return NextResponse.next();
 } 
