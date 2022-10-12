a4ee6a1e68c554cc3b0a25b66b707f8b34e5f3a9
tanvir <tanvirulislam149@gmail.com>
2022-10-13 00:21:16 +0600

    view doc in modal

--- components/Sections/UnapprovedList/UnapprovedList.js
+++ components/Sections/UnapprovedList/UnapprovedList.js
@@ -3,14 +3,14 @@ import styles from "./UnapprovedList.module.css";
 import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
 import Image from "next/image";
 import close from "../../../assets/Close.png";
-import { FormControl, MenuItem, Select, Switch } from "@mui/material";
+import { CircularProgress, FormControl, MenuItem, Select, Switch } from "@mui/material";
 import { viewUnapprovedDoc } from "../../../services/docsApproveService";
 
 const UnapprovedList = ({ searchResult }) => {
   const [number, setNumber] = useState(10);
-  const [docId, setDocId] = useState(0);
   const [docDetails, setDocDetails] = useState({});
   const [loading, setLoading] = useState(false);
+  const { id, title, organization, theme, sub_cat, stake_holder, value_chain, geography, status, state, city, language, description, keywords, citation, createdOn, attachment } = docDetails;
 
   const handleChange = (event) => {
     setNumber(event.target.value);
@@ -149,165 +149,168 @@ const UnapprovedList = ({ searchResult }) => {
                 </span>
               </div>
 
-              <div className={styles.cover}>
-                <div className={styles.content}>
-                  <h4 className={styles.questionDetailsTitle}>Question Details</h4>
-                  <div className={styles.documentDataCont}>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        Document Name
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>https://www.google.com/url?rct=j&sa=t&url=https://timesofindia.indiatimes.com/city/trichy/drda-picks-47-trichy-villages-to-make-them-odf-plus-model-places/articleshow/90652027.cms&ct=ga&cd=CAEYACoTNjc2MjM5ODI2MzUwMTEwNDIzMzIeMDJmYThkNjY5YjhjZWYyZDpjby5pbjplbjpJTjpS&usg=AOvVaw2qiQ909V1JLFhUCTYXW-Pi</p>
+              {loading ? <div className={styles.justify_center}><CircularProgress /></div> :
+                <>
+                  <div className={styles.cover}>
+                    <div className={styles.content}>
+                      <h4 className={styles.questionDetailsTitle}>Question Details</h4>
+                      <div className={styles.documentDataCont}>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            Document Name
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>null</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            Title
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{title}</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            Organization Name
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{organization}</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            Theme
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{theme}</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            Sub Category
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{sub_cat}</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            Stakeholder
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{stake_holder}</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            Value Chain
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{value_chain}</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            Geography
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{geography}</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            Urban / Rural
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{status}</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            State
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{state}</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            City
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{city}</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            Language
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{language}</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            Description
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{description}</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            Keyword
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{keywords}</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            Citation
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{citation}</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            Created On
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{createdOn}</p>
+                          </div>
+                        </div>
+                        <div className={styles.documentData}>
+                          <div className={styles.documentHeading}>
+                            View Document
+                          </div>
+                          <div className={styles.documentDetail}>
+                            <p>{attachment}</p>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        Title
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>DRDA Picks 47 Trichy Villages To Make Them Odf Plus Model Places</p>
-                      </div>
-                    </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        Organization Name
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>NFSSM</p>
-                      </div>
-                    </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        Theme
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>Latest FSSM News</p>
-                      </div>
-                    </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        Sub Category
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>555</p>
-                      </div>
-                    </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        Stakeholder
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>All</p>
-                      </div>
-                    </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        Value Chain
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>All</p>
-                      </div>
-                    </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        Geography
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>State</p>
-                      </div>
-                    </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        Urban / Rural
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>Rural</p>
-                      </div>
-                    </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        State
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>Tamil Nadu</p>
-                      </div>
-                    </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        City
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>Trichy</p>
-                      </div>
-                    </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        Language
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>English</p>
-                      </div>
-                    </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        Description
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>The District Rural Development Agency (DRDA) has selected 47 more villages in Trichy district to make them open defecation free (ODF) plus model ...</p>
-                      </div>
-                    </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        Keyword
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>AMRUT,Faecal Sludge and Septage Management,FSM,FSSM,FSTP,Open Defecation Free,Open Defecation Free and Sanitation,sanitation,Septage Management,Swachh Bharat Mission,Urban Local Bodies Faecal Sludge and Septage Management,Urban Sanitation,Water, Sanitation and Hygiene</p>
-                      </div>
-                    </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        Citation
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>Attachment</p>
-                      </div>
-                    </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        Created On
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>2022-04-07 12:16:07</p>
-                      </div>
-                    </div>
-                    <div className={styles.documentData}>
-                      <div className={styles.documentHeading}>
-                        View Document
-                      </div>
-                      <div className={styles.documentDetail}>
-                        <p>view</p>
-                      </div>
+                    <div className={styles.btn_cont}>
+                      <button className={`${styles.btn3} ${styles.save}`}>
+                        Edit Document
+                      </button>
+                      <button
+                        className={`${styles.btn3} ${styles.cancel}`}
+                        onClick={() => {
+                          document.querySelector(".m7").style.display = "none";
+                        }}
+                      >
+                        Delete Document
+                      </button>
+                      <button className={`${styles.btn3} ${styles.save}`}>
+                        Map Document
+                      </button>
                     </div>
                   </div>
-                </div>
-                <div className={styles.btn_cont}>
-                  <button className={`${styles.btn3} ${styles.save}`}>
-                    Edit Document
-                  </button>
-                  <button
-                    className={`${styles.btn3} ${styles.cancel}`}
-                    onClick={() => {
-                      document.querySelector(".m7").style.display = "none";
-                    }}
-                  >
-                    Delete Document
-                  </button>
-                  <button className={`${styles.btn3} ${styles.save}`}>
-                    Map Document
-                  </button>
-                </div>
-              </div>
+                </>}
             </div>
           </div>
         </> : ""}
