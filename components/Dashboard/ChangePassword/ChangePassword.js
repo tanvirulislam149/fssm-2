4595966e8b948c05886734ea3471fb0a752480b9
tanvir <tanvirulislam149@gmail.com>
2022-10-30 01:46:58 +0600

    change password UI done

--- components/Dashboard/ChangePassword/ChangePassword.js
+++ components/Dashboard/ChangePassword/ChangePassword.js
@@ -1,47 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ChangePassword.module.css';
 import { useRouter } from 'next/router';
 
 const ChangePassword = () => {
+  const [passErr, setPassErr] = useState("");
   const navigate = useRouter();
 
   const handleNav = () => {
-    navigate.back();
+    navigate.push('/dashboard');
   }
 
-  const handleSubmit = () => {
-
+  const handleSubmit = (e) => {
+    setPassErr("");
+    e.preventDefault();
+    const pass = e.target.pass.value;
+    const confirmPass = e.target.confirmPass.value;
+    const pattern = RegExp(
+      "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$"
+    );
+    if (pattern.test(pass)) {
+      if (pass === confirmPass) {
+        console.log("matched");
+      }
+      else {
+        setPassErr("Password didn't matched");
+      }
+    }
+    else {
+      setPassErr("Password should contain at least one uppercase, lowercase, number and character.")
+    }
   }
 
   return (
     <>
       <div className={styles.container}>
         <h4 className={styles.label2}>Change Password</h4>
-
         <form
           className={styles.cont}
-          onSubmit={() => {
-            handleSubmit();
+          onSubmit={(e) => {
+            handleSubmit(e);
           }}>
-          <div>
+          <div className={styles.inputCont1}>
             <label htmlFor="pass">New Password</label>
             <input
+              name='pass'
               required
               id="pass"
               minLength="6"
+              maxLength="20" c
               className={styles.input}
               type="password"
-              placeholder='New Password' />
+              placeholder='New Password'
+            />
           </div>
-          <div>
-            <label htmlFor="confirm">Confirm Password</label>
-            <input
-              required
-              id="confirm"
-              minLength="6"
-              className={styles.input}
-              type="password"
-              placeholder='Confirm Password' />
+          <div className={styles.inputCont2}>
+            <div>
+              <label htmlFor="confirm">Confirm Password</label>
+              <input
+                name='confirmPass'
+                required
+                id="confirm"
+                minLength="6"
+                maxLength="20"
+                className={styles.input}
+                type="password"
+                placeholder='Confirm Password' />
+            </div>
+            <p className={styles.error}>{passErr}</p>
           </div>
           <div className={styles.btn_cont}>
             <button
